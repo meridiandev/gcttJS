1a58509772df75054d1909f6d4c2b3575d3b0d27
sergeev <v.sergeev.m@gmail.com>
2021-02-18 23:36:52 +0300

    Update Task RU locales

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            Create Task
+            Создать задачу
         </h2>
     </x-slot>
 
@@ -12,7 +12,7 @@
                     @csrf
                     <div class="shadow overflow-hidden sm:rounded-md">
                         <div class="px-4 py-5 bg-white sm:p-6">
-                            <label for="description" class="block font-medium text-sm text-gray-700">Description</label>
+                            <label for="description" class="block font-medium text-sm text-gray-700">Описание</label>
                             <input type="text" name="description" id="description" type="text" class="form-input rounded-md shadow-sm mt-1 block w-full"
                                    value="{{ old('description', '') }}" />
                             @error('description')
@@ -22,7 +22,7 @@
 
                         <div class="flex items-center justify-end px-4 py-3 bg-gray-50 text-right sm:px-6">
                             <button class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray disabled:opacity-25 transition ease-in-out duration-150">
-                                Create
+                                Добавить
                             </button>
                         </div>
                     </div>
